c2bc3148f35de0d5f58bb1fb89809847338a2779
sandeep <106268680+morganthakur@users.noreply.github.com>
2023-07-21 17:42:53 +0530

    adding some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 
-import './App.css'
 import LiveScoresLeaderboard from './components/LiveScoresLeaderboard'
+import styled from "styled-components";
 
 function App() {
 
 
   return (
     <>
-   <h1 style={{textAlign:"center",fontSize:"1rem"}}   > Score Leaderboard </h1>
-   <div>
-    <LiveScoresLeaderboard />
-   </div>
+      <Heading style={{ textAlign: "center", fontSize: "1rem" }}>
+        {" "}
+        Score Leaderboard{" "}
+      </Heading>
+      <LeaderWrapper>
+        <LiveScoresLeaderboard />
+      </LeaderWrapper>
     </>
-  )
+  );
 }
+
+const Heading = styled.h1`
+text-align:center;
+font-size:1rem;
+`;
+const LeaderWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: rgb(250, 242, 242);
+`;
+
+
 
 export default App
